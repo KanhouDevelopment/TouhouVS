1711e4992119729524e67518b2f5c43a1cf7aaea
nac0n <lager_91@hotmail.com>
2018-11-18 16:12:01 +0100

    fixed

--- core/src/novus/gdx/game/GameHandler.java
+++ core/src/novus/gdx/game/GameHandler.java
@@ -9,16 +9,16 @@ import novus.gdx.screens.*;
 
 public class GameHandler extends Game{
 
-	SpriteBatch batch;
-	ControllerHandler ctrlHandler;
+	public SpriteBatch batch;
+	public ControllerHandler ctrlHandler;
 	
 	@Override
 	public void create() {
 		// TODO: Add creation
 		batch = new SpriteBatch();
 		Assets.loadTextures();
-		setScreen(new KanmonSplash(this));
 		ctrlHandler = new ControllerHandler();
+		setScreen(new KanhouSplash(this));
 	}
 	
 	@Override
